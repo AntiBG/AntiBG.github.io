9216fdab81bf22164058a64aa9dabe6028ea9ec4
Anti Bulgaria <47941342+AntiBG@users.noreply.github.com>
2019-06-23 09:05:48 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,17 @@
-function clickIE4(){
-	if (event.button==2){
-	return false;
-	}
-	}
-	function clickNS4(e){
-	if (document.layers||document.getElementById&&!document.all){
-	if (e.which==2||e.which==3){
-	return false;
-	}
-	}
-	}
-	if (document.layers){
-	document.captureEvents(Event.MOUSEDOWN);
-	document.onmousedown=clickNS4;
-	}
-	else if (document.all&&!document.getElementById){
-	document.onmousedown=clickIE4;
-	}
-	document.oncontextmenu=new Function("return false")
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-121281244-1');
 
-	video.addEventListener("canplaythrough", function() {
-		// Ready to play whole video?
-	  }, false);
+window.onload = () => {
+    let audio = document.getElementById("bgAudio");
+    audio.volume = 0.10;
+
+    var vid = document.getElementById("bgVideo");
+    var aud = document.getElementById("bgAudio");
+
+    aud.currentTime = vid.currentTime;
+    aud.play();
+
+    document.oncontextmenu = () => false;
+}
